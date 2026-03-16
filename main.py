4c07e859574cc6fa3e091acebd5a6acc5420bdd5
Enkhjin <2026064@miu.edu.mn>
2026-03-16 09:08:42 +0800

    import random

---
+++ main.py
@@ -0,0 +1,31 @@
+# import random 
+# coin = random.randint(0,1)
+
+# if coin == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+# import random 
+# die = random.randint(1,6)
+# die1 = random.randint(1,6)
+
+# print(f"Die1:{die}, Die2:{die1}")
+
+# students = ["Mendee", "yuki", "helly"] 
+# print(students)
+# students.append("josh") #adding
+# print(students)
+# students.remove("Mendee") #remove
+# print(students)
+
+# students[0] = "Yuki"
+# print(students)
+  
+
+movies = []
+
+print(movies)
+
+movies.append("stranger things")
+
+print(movies)
