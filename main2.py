48ba6c3f3015c3118fefc348513369c40c2f8867
Enkhjin <2026064@miu.edu.mn>
2026-03-19 15:20:23 +0800

    index

---
+++ main2.py
@@ -0,0 +1,62 @@
+#CRUD create read update delete
+
+# movies = []
+
+# movie = input("what's movie")
+
+# movies.append(movie)
+
+# print(movies)
+
+# stores = ["milk","bread"]
+
+# stores.append("butter")
+# stores.append("sugar")
+# stores.append("apple")
+# print(stores)
+
+# stores.remove("bread")
+# print(stores)
+
+# stores[2] = "brown sugar"
+# print(stores)
+
+# stores.sort()
+# print(stores)
+
+# print(len(stores))
+
+
+grades = [85,90,65,72]
+print(grades)
+
+# add = int(input("Add:"))
+# grades.append(add)
+# print(grades)
+ 
+# delete = int(input("Remove:"))
+# grades.remove(delete)
+# print(grades)
+  
+change = int(input("Which number to change: "))
+
+index = grades.index(change)
+
+grades[index] = 100
+
+print(grades)
+
+
+
+
+
+
+
+# grades = input("what's change")
+# grades = input("what's remove")
+
+# print(grades)
+# print(sum(grades))
+# print(len(grades))
+# average = sum(grades)/ len(grades)
+# print(average)
